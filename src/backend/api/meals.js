5ad47f5372f0158ab294cce2ce2dbc25ba23cd60
Divyajyothi Guttula <81112710+Divyajg@users.noreply.github.com>
2021-12-19 12:31:09 +0100

    Update meals.js

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
+//Returns all meal titles
+router.get("/titles", async(request, response) => {
+    try {
+        // knex syntax for selecting things. Look up the documentation for knex for further info
+        const titles = await knex("meals").select("title");
+        response.json(titles);
+    } catch (error) {
+        throw error;
+    }
+});
 //Adds a new meal
 router.post("/", async(request, response) => {
     try {
