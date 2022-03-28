53b346886554748248ba1a72aad72b7e008b0191
Divyajg <guttuladivyajyothi@gmail.com>
2022-03-28 15:56:13 +0200

    File created for further improvising

---
+++ src/client/components/AboutPageData.js
@@ -0,0 +1,34 @@
+export const AboutPageData = [{
+        title: "about us",
+        list: ["our history",
+            "news & notifications",
+            "franchise info",
+        ]
+    },
+    {
+        title: "services",
+        list: ["wifi",
+            "gift cards",
+            "play areas for children",
+            "party specials",
+            "family fun",
+            "online reservations",
+            "rewards",
+        ]
+    }, {
+        title: "community",
+        list: ["scholorships",
+            "m s charities",
+            "community info",
+            "m s international",
+        ]
+    }, {
+        title: "contact us",
+        list: ["gift card FAQs",
+            "donations",
+            "employment",
+            "feedback",
+            "FAQs",
+        ]
+    }
+]
